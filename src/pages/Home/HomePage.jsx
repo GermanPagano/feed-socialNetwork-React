4d255dafc0d489c6946b969698218cb56f155237
germanpagano <sergiogermanpagano@gmail.com>
2023-05-05 18:37:03 -0300

    update background

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,16 +1,17 @@
-import React, { useContext , useState } from "react";
+import React, { useContext, useState } from "react";
 import "../Home/HomepageStyles.css";
 import { UserContext } from "../../storage/UserContext";
 import { useNavigate } from "react-router-dom";
 //import AuthFB from "../../Auth/Auth-facebook/AuthFB";
 import AuthManual from "../../Auth/Auth-manual/AuthManual";
 import Login from "../../Auth/Login/Login";
+import videoBackground from "../../assets/bg-app.mp4";
 
 function HomePage() {
   const context = useContext(UserContext);
   const { username } = context.userData;
   const navigate = useNavigate();
-  
+
   const [isLoginVisible, setLoginVisible] = useState(true);
   const [isRegisterVisible, setRegisterVisible] = useState(false);
 
@@ -26,59 +27,85 @@ function HomePage() {
 
   return (
     <div className="fullview  ">
+      <video autoPlay muted loop id="bg-video">
+        <source src={videoBackground} type="video/mp4" />
+      </video>
 
       {username !== "" ? (
-        navigate(`/feed/${username}`) 
+        navigate(`/feed/${username}`)
       ) : (
         <div>
-        <div>
-        {/* Renderizar el componente de inicio de sesión si es visible */}
-        {isLoginVisible && (
-          <> 
-          <div className="row" style={{display:'flex', justifyContent:'center' , marginTop:'1.8rem'}}>
-            <h4> Ingresa con tu cuenta </h4>
-            <Login/>
+          <div>
+            {/* Renderizar el componente de inicio de sesión si es visible */}
+            {isLoginVisible && (
+              <>
+                <div
+                  className="row"
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    marginTop: "1.8rem",
+                  }}
+                >
+                  <h4> Ingresa con tu cuenta </h4>
+                  <Login />
+                </div>
+
+                <div
+                  style={{
+                    zIndex: "1",
+                    position:'relative'
+                  }}
+                >
+                  <p>
+
+                    ¿No tenes cuenta?
+                    <button
+                      style={{
+                        color: "white ",
+                        background: "none",
+                        border: "none",
+                        padding: "0",
+                        margin: "10px",
+                        cursor: "pointer",
+                        fontWeight: "bold",
+                      }}
+                      onClick={handleRegisterClick}
+                    >
+                      Registrate
+                    </button>
+                  </p>
+                </div>
+              </>
+            )}
           </div>
 
-  
-  
-        <p> ¿ No tenes cuenta ? 
-        <button style={{
-              color: 'white ',
-              background: 'none',
-              border: 'none',
-              padding: '0',
-              margin: '10px',
-              cursor: 'pointer',
-              fontWeight:'bold',
-            }} onClick={handleRegisterClick}>
-            Registrate
-          </button> 
-          </p>
-          </>
-          )}
-        </div>
-
-      {/* Renderizar el componente de registro si es visible */}
-      {isRegisterVisible && (
-          <div className="login-options mt-4 ">
-          <h4> Crea tu cuenta  </h4>
-          <br/>
-            <AuthManual/>
-          <p> ¿ Tenes cuenta ? 
-          <button style={{
-              color: 'white ',
-              background: 'none',
-              border: 'none',
-              padding: '0',
-              margin: '10px',
-              cursor: 'pointer',
-              fontWeight:'bold',
-            }} onClick={handleLoginClick}>
-            Ingresa
-          </button> </p>
-            {/* <AuthFB /> */}
-          </div>
+          {/* Renderizar el componente de registro si es visible */}
+          {isRegisterVisible && (
+            <div className="login-options mt-4 ">
+              <h4> Crea tu cuenta </h4>
+              <br />
+              <AuthManual />
+              <p>
+                {" "}
+                ¿ Tenes cuenta ?
+                <button
+                  style={{
+                    color: "white ",
+                    background: "none",
+                    border: "none",
+                    padding: "0",
+                    margin: "10px",
+                    cursor: "pointer",
+                    fontWeight: "bold",
+                  }}
+                  onClick={handleLoginClick}
+                >
+                  Ingresa
+                </button>{" "}
+              </p>
+              {/* <AuthFB /> */}
+            </div>
           )}
         </div>
       )}
